b587055cb4e9e98ed726956ebd556f57b1f9b3a5
S Nawaz <95207462+SNawazmac@users.noreply.github.com>
2023-04-13 11:41:45 +0530

    Update Invoke_Failover_Storage_multi-values_param.ps1

--- Invoke_Failover_Storage_multi-values_param.ps1
+++ Invoke_Failover_Storage_multi-values_param.ps1
@@ -8,11 +8,14 @@ param(
 $storageaccounts = $storageaccount_names.split(",")
 foreach($storageaccount_name in $storageaccounts)
 {
-###Set-AzStorageAccount -ResourceGroupName $resource_group_name -Name $storageaccount_name -SkuName $sku -Force
 #Below command Invokes failover on the selected storage account(s)
-Invoke-AzStorageAccountFailover -ResourceGroupName $resource_group_name -Name $storageaccount_name -Force -AsJob
+$failover = Invoke-AzStorageAccountFailover -ResourceGroupName $resource_group_name -Name $storageaccount_name -Force -AsJob
+}
 
+Wait-Job $failover
+
+foreach($storageaccount_name in $storageaccounts)
+{
 #Below command updates the SKU of the storage account(s) to Standard_GRS post failover
-#Set-AzStorageAccount -ResourceGroupName $resource_group_name -Name $storageaccount_name -SkuName $sku -Force
-#Write-Host $storageaccount_name 
+Set-AzStorageAccount -ResourceGroupName $resource_group_name -Name $storageaccount_name -SkuName $sku -Force
 }
