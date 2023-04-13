5982df375091b9c8c8daad9dff4bee9a7e344e57
S Nawaz <95207462+SNawazmac@users.noreply.github.com>
2023-04-13 11:11:51 +0530

    Update and rename powershell.ps1 to Invoke_Failover_Storage_multi-values_param.ps1

--- Invoke_Failover_Storage_multi-values_param.ps1
+++ Invoke_Failover_Storage_multi-values_param.ps1
@@ -10,12 +10,9 @@ foreach($storageaccount_name in $storageaccounts)
 {
 ###Set-AzStorageAccount -ResourceGroupName $resource_group_name -Name $storageaccount_name -SkuName $sku -Force
 #Below command Invokes failover on the selected storage account(s)
-Invoke-AzStorageAccountFailover -ResourceGroupName $resource_group_name -Name $storageaccount_name -Force
-
- write-host $storageaccount_name
- write-host "Next account"
+Invoke-AzStorageAccountFailover -ResourceGroupName $resource_group_name -Name $storageaccount_name -Force -AsJob
 
 #Below command updates the SKU of the storage account(s) to Standard_GRS post failover
-#Set-AzStorageAccount -ResourceGroupName $resource_group_name -Name $storageaccount_name -SkuName $sku -Force
+Set-AzStorageAccount -ResourceGroupName $resource_group_name -Name $storageaccount_name -SkuName $sku -Force
 #Write-Host $storageaccount_name 
 }
