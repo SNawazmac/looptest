5a0b68963f34193d2377983f7574f4f1197eb088
S Nawaz <95207462+SNawazmac@users.noreply.github.com>
2023-04-13 17:03:28 +0530

    Add files via upload

---
+++ Invoke_SQL_Failover_Multi_param.ps1
@@ -0,0 +1,22 @@
+﻿param(
+[Parameter(Mandatory=$true)][string]$resource_group_name,              #Enter the name of resource group where the secondary sql server is existing
+[Parameter(Mandatory=$true)][array]$secondary_server_names,            #Enter the secondary server name
+[Parameter(Mandatory=$true)][array]$failover_group_names,                      #Enter the failovergroupname
+[Parameter(Mandatory=$true)][string]$secondary_subscription_Id                 #Enter the secondary_subscription_Id                            
+)
+
+$secondaryservernames_split = $secondary_server_names.split(",")
+$failovergroupnames_split = $failover_group_names.split(",")
+
+foreach($secondary_server_name in $secondaryservernames_split){
+
+foreach($failover_group_name in $failovergroupnames_split){
+
+#The below command sets authentication information for cmdlets that run in the current session
+Set-AzContext -Subscription $secondary_subscription_Id
+
+#The below command invokes failover from primary to secondary sql server in the failover group
+Switch-AzSqlDatabaseFailoverGroup -ResourceGroupName $resource_group_name -ServerName $secondary_server_name -FailoverGroupName $failover_group_name -AsJob
+
+}
+}
